fd135192b2482924007928323dc14a8b6e2324bb
Taeuk Gwak <gwak2837@kakao.com>
2022-08-03 21:49:22 +0900

    Add post sql

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -4,6 +4,9 @@ import * as certMutationResolver from './cert/Mutation'
 import * as certrObjectResolver from './cert/Object'
 import * as certQueryResolver from './cert/Query'
 import * as commonResolver from './common/common'
+// import * as postMutationResolver from './post/Mutation'
+// import * as postObjectResolver from './post/Object'
+import * as postQueryResolver from './post/Query'
 import * as userMutationResolver from './user/Mutation'
 import * as userObjectResolver from './user/Object'
 import * as userQueryResolver from './user/Query'
@@ -13,6 +16,9 @@ const resolversArray = [
   certrObjectResolver,
   certQueryResolver,
   commonResolver,
+  // postMutationResolver,
+  // postObjectResolver,
+  postQueryResolver,
   userMutationResolver,
   userObjectResolver,
   userQueryResolver,
