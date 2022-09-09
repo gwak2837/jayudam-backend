c1792dbd655c7bf367e5e4982f28e50ae7075baa
Taeuk Gwak <gwak2837@kakao.com>
2022-09-09 21:37:08 +0900

    Update Object.ts

--- src/graphql/user/Object.ts
+++ src/graphql/user/Object.ts
@@ -1,3 +1,5 @@
+import { UserResolvers } from '../generated/graphql'
+
 export const Sex = {
   UNKNOWN: 0,
   MALE: 1,
@@ -9,4 +11,12 @@ export const Grade = {
   FREE: 0,
   PRO: 1,
   ENTERPRISE: 2,
+}
+
+export const User: UserResolvers = {
+  towns: ({ towns }) => {
+    const exactTowns = towns?.filter((town) => town.name)
+
+    return exactTowns && exactTowns.length > 0 ? exactTowns : null
+  },
 }
